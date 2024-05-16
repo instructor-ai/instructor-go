3bbe6ddd4a259dff5275a0da23ff5862ad6134c8
Sushmitha Mallesh <sushdec6@gmail.com>
2024-05-16 21:25:19 +0530

    openai: Support for Images in Message (#12)
    * Support for Images in message-OpenAi
    
    * fix image url
    
    * Revert "fix image url"
    
    This reverts commit cee175354aeec82e029b34817dd5e359c1edf998.
    
    * bump openai and use GPT-4o
    
    * fix role bug
    
    ---------
    
    Co-authored-by: Robby <h0rv@users.noreply.github.com>

---
+++ examples/image/open_ai_image_in_message.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/instructor-ai/instructor-go/pkg/instructor"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type Book struct {
+	Title  string  `json:"title,omitempty"  jsonschema:"title=title,description=The title of the book,example=Harry Potter and the Philosopher's Stone"`
+	Author *string `json:"author,omitempty" jsonschema:"title=author,description=The author of the book,example=J.K. Rowling"`
+}
+
+type BookCatalog struct {
+	Catalog []Book `json:"catalog"`
+}
+
+func (bc *BookCatalog) PrintCatalog() {
+	fmt.Printf("Number of books in the catalog: %d\n\n", len(bc.Catalog))
+	for _, book := range bc.Catalog {
+		fmt.Printf("Title:  %s\n", book.Title)
+		fmt.Printf("Author: %s\n", *book.Author)
+		fmt.Println("--------------------")
+	}
+}
+
+func main() {
+	ctx := context.Background()
+
+	client, err := instructor.FromOpenAI[BookCatalog](
+		openai.NewClient(os.Getenv("OPENAI_API_KEY")),
+		instructor.WithMode(instructor.ModeJSON),
+		instructor.WithMaxRetries(3),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	url := "https://utfs.io/f/fe55d6bd-e920-4a6f-8e93-a4c9dd851b90-eivhb2.png"
+
+	bookCatalog, err := client.CreateChatCompletion(
+		ctx,
+		instructor.Request{
+			Model: openai.GPT4o,
+			Messages: []instructor.Message{
+				{
+					Role: instructor.RoleUser,
+					MultiContent: []instructor.ChatMessagePart{
+						{
+							Type: instructor.ChatMessagePartTypeText,
+							Text: "Extract book catelog from the image",
+						},
+						{
+							Type: instructor.ChatMessagePartTypeImageURL,
+							ImageURL: &instructor.ChatMessageImageURL{
+								URL: url,
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	bookCatalog.PrintCatalog()
+	/*
+		Number of books in the catalog: 15
+
+		Title:  Pride and Prejudice
+		Author: Jane Austen
+		--------------------
+		Title:  The Great Gatsby
+		Author: F. Scott Fitzgerald
+		--------------------
+		Title:  The Catcher in the Rye
+		Author: J. D. Salinger
+		--------------------
+		Title:  Don Quixote
+		Author: Miguel de Cervantes
+		--------------------
+		Title:  One Hundred Years of Solitude
+		Author: Gabriel García Márquez
+		--------------------
+		Title:  To Kill a Mockingbird
+		Author: Harper Lee
+		--------------------
+		Title:  Beloved
+		Author: Toni Morrison
+		--------------------
+		Title:  Ulysses
+		Author: James Joyce
+		--------------------
+		Title:  Harry Potter and the Cursed Child
+		Author: J.K. Rowling
+		--------------------
+		Title:  The Grapes of Wrath
+		Author: John Steinbeck
+		--------------------
+		Title:  1984
+		Author: George Orwell
+		--------------------
+		Title:  Lolita
+		Author: Vladimir Nabokov
+		--------------------
+		Title:  Anna Karenina
+		Author: Leo Tolstoy
+		--------------------
+		Title:  Moby-Dick
+		Author: Herman Melville
+		--------------------
+		Title:  Wuthering Heights
+		Author: Emily Brontë
+		--------------------
+	*/
+}
